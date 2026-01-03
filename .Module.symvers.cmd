794b7b0f9ebcf25cdded53f94cfeb852c03140cf
Phạm Trung Hiếu <161118087+hieup2003191111@users.noreply.github.com>
2026-01-03 16:57:55 +0700

    Delete .Module.symvers.cmd

--- .Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-savedcmd_/home/hieu/hoclinux/led/Module.symvers :=  scripts/mod/modpost -M -m -a      -o /home/hieu/hoclinux/led/Module.symvers -n -T /home/hieu/hoclinux/led/modules.order -i Module.symvers -e 
